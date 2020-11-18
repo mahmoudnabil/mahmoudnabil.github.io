96bb1ae0f36bb596904ea120a663e684f557aff4
mahmoudnabil <mah.nabil@yahoo.com>
2020-11-18 09:23:36 -0500

    commit message

---
+++ Teaching/ECEN885F20/protect.py
@@ -0,0 +1,22 @@
+import PyPDF2 as p,os
+
+
+
+path = os.getcwd()+"/Teaching/ECEN885F20/"
+files = os.listdir(path)
+files =sorted(files)
+
+print(files)
+
+for file_ in files:
+    if file_.endswith(".pdf"):
+        input_stream = p.PdfFileReader(open(path+file_, "rb"))
+        output = p.PdfFileWriter()
+        for i in range(0, input_stream.getNumPages()):
+            output.addPage(input_stream.getPage(i))
+
+        outputstream = open(path+"protected/"+file_, "wb")
+
+        output.encrypt("ECEN885F20", use_128bit=True)
+        output.write(outputstream)
+        outputstream.close()
